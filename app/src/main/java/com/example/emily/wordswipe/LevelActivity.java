01a190f1203562987cd1e316a95c933879e863a3
Emlay9 <46056494+Emlay9@users.noreply.github.com>
2019-04-10 09:50:10 -0600

    register onmove on letters

--- app/src/main/java/com/example/emily/wordswipe/LevelActivity.java
+++ app/src/main/java/com/example/emily/wordswipe/LevelActivity.java
@@ -2,9 +2,11 @@ package com.example.emily.wordswipe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 // OnTouchListener implemented to register touch events (in this case the swiping) on the
 // custom View (the swipe circle where you pick words)
@@ -15,6 +17,16 @@ public class LevelActivity extends AppCompatActivity implements View.OnTouchList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
 
+        // All the textviews for the letters that you can swipe to make words with
+        TextView topLetter = (TextView)findViewById(R.id.top_letter_choice);
+        TextView topLeftLetter = (TextView)findViewById(R.id.left_top_letter_choice);
+        TextView topRightLetter = (TextView)findViewById(R.id.right_top_letter_choice);
+        TextView bottomLeftLetter = (TextView)findViewById(R.id.left_bottom_letter_choice);
+        TextView bottomRightLetter = (TextView)findViewById(R.id.right_bottom_letter_choice);
+
+
+        topLetter.setOnTouchListener(this);
+        topLeftLetter.setOnTouchListener(this);
     }
 
     /***** onTouch(View v, MotionEvent event) info *****/
@@ -25,9 +37,24 @@ public class LevelActivity extends AppCompatActivity implements View.OnTouchList
      where its being pressed, if it's moving, where it stops etc etc...)
      */
     @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        event.getAction();
-        return false;
+    public boolean onTouch(View view, MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_MOVE)
+        {
+            switch(view.getId()) {
+                case R.id.top_letter_choice:
+                {
+                    Toast.makeText(this, "top letter", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                case R.id.left_top_letter_choice:
+                {
+                    Toast.makeText(this, "top left letter", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+
+            }
+        }
+        return true;
     }
 
 
--- app/src/main/java/com/example/emily/wordswipe/TitleScreenActivity.java
+++ app/src/main/java/com/example/emily/wordswipe/TitleScreenActivity.java
@@ -1,13 +1,26 @@
 package com.example.emily.wordswipe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class TitleScreenActivity extends AppCompatActivity {
+public class TitleScreenActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title_screen);
+
+        Button playBtn = (Button) findViewById(R.id.play_btn);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TitleScreenActivity.this, LevelActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
