177ce91e8c202d81dc7ad25177fccf5cc9d6103c
Emlay9 <46056494+Emlay9@users.noreply.github.com>
2019-04-10 16:24:45 -0600

    one view, find coord

--- app/src/main/java/com/example/emily/wordswipe/LevelActivity.java
+++ app/src/main/java/com/example/emily/wordswipe/LevelActivity.java
@@ -20,28 +20,32 @@ public class LevelActivity extends AppCompatActivity implements View.OnTouchList
     TextView topRightLetter;
     TextView bottomLeftLetter;
     TextView bottomRightLetter;
+    TextView swipeCircle;
 
     TextView firstLetterPicked;
     TextView secondLetterPicked;
     TextView thirdLetterPicked;
-
+    int countLettersPicked = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
-
+        countLettersPicked = 0;
         // All the textviews for the letters that you can swipe to make words with
-        topLetter = (TextView)findViewById(R.id.top_letter_choice);
-        topLeftLetter = (TextView)findViewById(R.id.left_top_letter_choice);
-        topRightLetter = (TextView)findViewById(R.id.right_top_letter_choice);
-        bottomLeftLetter = (TextView)findViewById(R.id.left_bottom_letter_choice);
-        bottomRightLetter = (TextView)findViewById(R.id.right_bottom_letter_choice);
+//        topLetter = (TextView)findViewById(R.id.top_letter_choice);
+//        topLeftLetter = (TextView)findViewById(R.id.left_top_letter_choice);
+//        topRightLetter = (TextView)findViewById(R.id.right_top_letter_choice);
+//        bottomLeftLetter = (TextView)findViewById(R.id.left_bottom_letter_choice);
+//        bottomRightLetter = (TextView)findViewById(R.id.right_bottom_letter_choice);
+        swipeCircle = (TextView)findViewById(R.id.letter_choice_circle);
+        swipeCircle.setOnTouchListener(this);
+
 
         //Set on touch listener for all of the swipeable letter textviews
-        topLetter.setOnTouchListener(this);
-        topLeftLetter.setOnTouchListener(this);
-        topRightLetter.setOnTouchListener(this);
-        bottomLeftLetter.setOnTouchListener(this);
+//        topLetter.setOnTouchListener(this);
+//        topLeftLetter.setOnTouchListener(this);
+//        topRightLetter.setOnTouchListener(this);
+//        bottomLeftLetter.setOnTouchListener(this);
     }
 
     /***** onTouch(View v, MotionEvent event) info *****/
@@ -54,7 +58,7 @@ public class LevelActivity extends AppCompatActivity implements View.OnTouchList
      */
     @Override
     public boolean onTouch(View view, MotionEvent event) {
-        int countLettersPicked = 0;
+
         Boolean topLetterPicked = false;
         Boolean topLeftLetterPicked = false;
         List<String> lettersPicked = new ArrayList<>();
@@ -64,32 +68,41 @@ public class LevelActivity extends AppCompatActivity implements View.OnTouchList
 
         // ACTION_MOVE means when your finger is dragging across the screen
             if(event.getAction() == MotionEvent.ACTION_MOVE ) {
-                while (event.getAction() != MotionEvent.ACTION_UP) {
+                float x = event.getX();
+                float y = event.getY();
+
+                //top letter x=
+
+//                while (event.getAction() != MotionEvent.ACTION_UP) {
                     switch (view.getId()) {
-                        case R.id.top_letter_choice: {
-                            if (topLetterPicked == false) {
-                                lettersPicked = addLetterChoice(lettersPicked, countLettersPicked, topLetter.getText().toString());
-                                displayLetterPicker(lettersPicked);
+                        case R.id.letter_choice_circle: {
+                                Toast.makeText(this, String.valueOf(x) + ", " + String.valueOf(y), Toast.LENGTH_SHORT).show();
                                 countLettersPicked++;
-                                topLetterPicked = true;
-                            }
+
+//                            if (topLetterPicked == false) {
+//                                lettersPicked = addLetterChoice(lettersPicked, countLettersPicked, topLetter.getText().toString());
+//                                displayLetterPicker(lettersPicked);
+//                                countLettersPicked++;
+
+//                                topLetterPicked = true;
+//                            }
                             break;
                         }
-                        case R.id.left_top_letter_choice: {
-//                            Toast.makeText(this, "top left letter", Toast.LENGTH_SHORT).show();
-//                            firstLetterPicked = (TextView)findViewById(R.id.first_letter_picked);
-//                            firstLetterPicked.setText("S");
-                            if (!topLeftLetterPicked) {
-                                lettersPicked = addLetterChoice(lettersPicked, countLettersPicked, topLetter.getText().toString());
-                                displayLetterPicker(lettersPicked);
-                                countLettersPicked++;
-                                topLeftLetterPicked = true;
-                            }
-                            break;
-                        }
+//                        case R.id.left_top_letter_choice: {
+//                                Toast.makeText(this, "top left letter", Toast.LENGTH_SHORT).show();
+////                            firstLetterPicked = (TextView)findViewById(R.id.first_letter_picked);
+////                            firstLetterPicked.setText("S");
+////                            if (!topLeftLetterPicked) {
+////                                lettersPicked = addLetterChoice(lettersPicked, countLettersPicked, topLeftLetter.getText().toString());
+////                                displayLetterPicker(lettersPicked);
+////                                countLettersPicked++;
+//                                topLeftLetterPicked = true;
+//                            }
+//                            break;
+//                        }
                     }//end switch
                 }//end if
-            }
+//            }
         return true;
     }
 
