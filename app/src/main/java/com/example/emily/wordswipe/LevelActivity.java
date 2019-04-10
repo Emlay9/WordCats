b4286a9c4bc02fae309c376a063c34637ff90734
Emlay9 <46056494+Emlay9@users.noreply.github.com>
2019-04-09 23:49:02 -0600

    circle for letter picking

--- app/src/main/java/com/example/emily/wordswipe/LevelActivity.java
+++ app/src/main/java/com/example/emily/wordswipe/LevelActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 
 // OnTouchListener implemented to register touch events (in this case the swiping) on the
 // custom View (the swipe circle where you pick words)
@@ -13,6 +14,7 @@ public class LevelActivity extends AppCompatActivity implements View.OnTouchList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
+
     }
 
     /***** onTouch(View v, MotionEvent event) info *****/
@@ -20,11 +22,11 @@ public class LevelActivity extends AppCompatActivity implements View.OnTouchList
        - It's similar to the onClick() method for buttons
      It's parameters are: the View v being touched and
         MotionEvent event gives information about the touch on that view (example: coordinates of
-     it
+     where its being pressed, if it's moving, where it stops etc etc...)
      */
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-
+        event.getAction();
         return false;
     }
 
