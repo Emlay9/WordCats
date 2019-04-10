9695d4751e51aca0c9a359e1207946583a3d6ade
Emlay9 <46056494+Emlay9@users.noreply.github.com>
2019-04-09 22:40:18 -0600

    all words filled out, on touch listener added

--- app/src/main/java/com/example/emily/wordswipe/LevelActivity.java
+++ app/src/main/java/com/example/emily/wordswipe/LevelActivity.java
@@ -2,12 +2,31 @@ package com.example.emily.wordswipe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
-public class LevelActivity extends AppCompatActivity {
+// OnTouchListener implemented to register touch events (in this case the swiping) on the
+// custom View (the swipe circle where you pick words)
+public class LevelActivity extends AppCompatActivity implements View.OnTouchListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
     }
+
+    /***** onTouch(View v, MotionEvent event) info *****/
+    /* - This method needs to be added when we implement View.OnTouchListener
+       - It's similar to the onClick() method for buttons
+     It's parameters are: the View v being touched and
+        MotionEvent event gives information about the touch on that view (example: coordinates of
+     it
+     */
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        return false;
+    }
+
+
 }
