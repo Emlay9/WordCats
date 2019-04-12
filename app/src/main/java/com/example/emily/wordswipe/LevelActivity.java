a82e5e3a245b10de362fc692cf5c7cc9b53d4c20
Emlay9 <46056494+Emlay9@users.noreply.github.com>
2019-04-12 10:55:14 -0600

    fixed running issue

--- app/src/main/java/com/example/emily/wordswipe/LevelActivity.java
+++ app/src/main/java/com/example/emily/wordswipe/LevelActivity.java
@@ -1,5 +1,6 @@
 package com.example.emily.wordswipe;
 
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,22 +11,40 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.os.Handler;
+import java.util.logging.LogRecord;
 
 // OnTouchListener implemented to register touch events (in this case the swiping) on the
 // custom View (the swipe circle where you pick words)
+
 public class LevelActivity extends AppCompatActivity implements View.OnTouchListener
 {
-    TextView topLetter;
-    TextView topLeftLetter;
-    TextView topRightLetter;
-    TextView bottomLeftLetter;
-    TextView bottomRightLetter;
+
+//    TextView topLetter;
+//    TextView topLeftLetter;
+//    TextView topRightLetter;
+//    TextView bottomLeftLetter;
+//    TextView bottomRightLetter;
     TextView swipeCircle;
 
+    Boolean topLetterPicked = false;
+    Boolean topLeftLetterPicked = false;
+    Boolean bottomLeftLetterPicked = false;
+    Boolean topRightLetterPicked = false;
+    Boolean bottomRightLetterPicked = false;
+
+    //TextViews of the letters picked that shows up above the circle
     TextView firstLetterPicked;
     TextView secondLetterPicked;
     TextView thirdLetterPicked;
+    TextView fourthLetterPicked;
+    TextView fifthLetterPicked;
+
     int countLettersPicked = 0;
+    List<String> lettersPicked = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,80 +78,96 @@ public class LevelActivity extends AppCompatActivity implements View.OnTouchList
     @Override
     public boolean onTouch(View view, MotionEvent event) {
 
-        Boolean topLetterPicked = false;
-        Boolean topLeftLetterPicked = false;
-        List<String> lettersPicked = new ArrayList<>();
-        String firstLetter;
-        String secondLetter;
-        int countLettersPicked = 0;
 
-
-        // ACTION_MOVE means when your finger is dragging across the screen
-        // ACTION_UP means when you stop touching the screen
-        switch(event.getAction())
-        {
-            case MotionEvent.ACTION_MOVE:
-            {
-                float x = event.getX();
-                float y = event.getY();
-                if (view.getId() == R.id.letter_choice_circle) {
-                    Toast.makeText(this, String.valueOf(x) + ", " + String.valueOf(y), Toast.LENGTH_SHORT).show();
-                    while(countLettersPicked <= 2)
-                    {
-                        getLettersPicked(x, y, countLettersPicked);
-                        countLettersPicked++;
+        if (view.getId() == R.id.letter_choice_circle) {
+            switch (event.getAction()) {
+                // ACTION_MOVE means when your finger is dragging across the screen
+                // During this time we want to give feedback to the user to show what letters they're selecting
+                case MotionEvent.ACTION_MOVE: {
+                    //get the coordinates of where on the circle is being touched
+                    float x = event.getX();
+                    float y = event.getY();
+                    // Uncomment the following line of code for demo of how to see the coordinates on the circle
+//                    Toast.makeText(this, String.valueOf(x) + ", " + String.valueOf(y), Toast.LENGTH_SHORT).show();
+                    if (countLettersPicked <= 2) {
+                        countLettersPicked = getLettersPicked(x, y, countLettersPicked);
                     }
+                    break;
+                }
+//                ACTION_UP means when you stop touching the screen
+//                 when this happens we want to check if the swiped word is a match and clear the word selection that pops up as you swipe
+                case MotionEvent.ACTION_UP: {
+                    checkForMatch();
+                    clearSelectedLetters();
+                    countLettersPicked = 0;
+                    topLetterPicked = false;
+                    topLeftLetterPicked = false;
 
                 }
-            }
-            case MotionEvent.ACTION_UP:
-            {
-                checkForMatch();
-//                clearSelectedLetters();
             }
         }
         return true;
     }
 
-    private void getLettersPicked(float x, float y, int countLettersPicked) {
-        Boolean topLetterPicked = false;
-        Boolean topLeftLetterPicked = false;
+    private int getLettersPicked(float x, float y, int countLettersPicked) {
         String topLetter = "E";
         String topLeftLetter = "S";
-        List<String> lettersPicked = new ArrayList<>();
 
-//        while(countLettersPicked <= 2)
-//        {
-            if(x < 120 && y < 365 && y > 230)
+            // coordinate areas on the circle corresponding to where the 5 letters are
+            if(x < 120 && y < 365 && y > 230 && topLeftLetterPicked == false)
             {
                 lettersPicked.add(countLettersPicked, topLeftLetter);
                 topLeftLetterPicked = true;
                 countLettersPicked++;
                 displayLetterChoice(lettersPicked, countLettersPicked);
             }
-            if(x > 290 && x < 450 && y < 180)
+            if(x > 290 && x < 450 && y < 180 && topLetterPicked == false)
             {
                 lettersPicked.add(countLettersPicked, topLetter);
                 topLetterPicked = true;
                 countLettersPicked++;
                 displayLetterChoice(lettersPicked, countLettersPicked);
             }
-//        }
+            if(x > 175 && x < 250 && y > 555)
+            {
+
+            }
+
+        return countLettersPicked;
     }
 
     private void displayLetterChoice(List<String> lettersPicked, int countLettersPicked) {
 
-        if(countLettersPicked == 1)
-        {
-            firstLetterPicked = (TextView)findViewById(R.id.first_letter_picked);
-            firstLetterPicked.setVisibility(View.VISIBLE);
-            firstLetterPicked.setText(lettersPicked.get(0));
-        }
-        if(countLettersPicked == 2)
-        {
-            secondLetterPicked = (TextView)findViewById(R.id.second_letter_picked);
-            secondLetterPicked.setVisibility(View.VISIBLE);
-            secondLetterPicked.setText(lettersPicked.get(1));
+        switch (countLettersPicked) {
+            case 1:
+            {
+                firstLetterPicked = (TextView)findViewById(R.id.first_letter_picked);
+                firstLetterPicked.setVisibility(View.VISIBLE);
+                firstLetterPicked.setText(lettersPicked.get(0));
+                break;
+            }
+            case 2:
+            {
+                secondLetterPicked = (TextView)findViewById(R.id.second_letter_picked);
+                secondLetterPicked.setVisibility(View.VISIBLE);
+                secondLetterPicked.setText(lettersPicked.get(1));
+                break;
+            }
+            case 3:
+            {
+
+                break;
+            }
+            case 4:
+            {
+
+                break;
+            }
+            case 5:
+            {
+
+                break;
+            }
         }
     }
 
@@ -140,13 +175,26 @@ public class LevelActivity extends AppCompatActivity implements View.OnTouchList
 
     }
 
+
+
     private void clearSelectedLetters() {
-        firstLetterPicked = (TextView)findViewById(R.id.first_letter_picked);
-        firstLetterPicked.setVisibility(View.GONE);
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                firstLetterPicked = (TextView)findViewById(R.id.first_letter_picked);
+                firstLetterPicked.setVisibility(View.INVISIBLE);
 
-        secondLetterPicked = (TextView)findViewById(R.id.second_letter_picked);
-        secondLetterPicked.setVisibility(View.GONE);
+                secondLetterPicked = (TextView)findViewById(R.id.second_letter_picked);
+                secondLetterPicked.setVisibility(View.INVISIBLE);
+            }
+        }, 500);
+
+//        new Timer().schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        }, 2000);
+
     }
-
-
 }
